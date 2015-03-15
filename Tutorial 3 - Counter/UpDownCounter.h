45e43f1732166c4d69f438cd9ebe832d24bcbd92
Ebot-Monchey <ebot.ojong@gmail.com>
2015-03-15 17:14:13 +0200

    Declared the UpDownCounter class

---
+++ Tutorial 3 - Counter/UpDownCounter.h
@@ -0,0 +1,15 @@
+class UpDownCounter {
+private:
+	int maxV;		// Stores the maximum value that the counter can hold
+	int minV;		// The minimum value that the counter can hold
+	int incr;		// The step of the increment
+	int decr;		// The step of the decrement
+public:
+	UpDownCounter();
+	UpDownCounter(int maxV, int minV, int incr, int decr);
+	int getValue();
+	void operator++();
+	void operator++(int dummy);
+	void operator--();
+	void operator--(int dummy);
+};
