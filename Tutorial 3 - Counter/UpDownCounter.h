4ca89c80da3a536843a23f386277498670174544
Ebot-Monchey <ebot.ojong@gmail.com>
2015-03-15 17:45:36 +0200

    Added header guard

--- Tutorial 3 - Counter/UpDownCounter.h
+++ Tutorial 3 - Counter/UpDownCounter.h
@@ -1,16 +1,19 @@
-class UpDownCounter {
-private:
-	int maxV;		// Stores the maximum value that the counter can hold
-	int minV;		// The minimum value that the counter can hold
-	int incr;		// The step of the increment
-	int decr;		// The step of the decrement
-	int value;		// The value of the counter
-public:
-	UpDownCounter();
-	UpDownCounter(int maxV, int minV, int incr, int decr);
-	int getValue();
-	void operator++();
-	void operator++(int dummy);
-	void operator--();
-	void operator--(int dummy);
-};
+#ifndef UPDOWNCOUNTER_H
+#define UPDOWNCOUNTER_H
+	class UpDownCounter {
+	private:
+		int maxV;		// Stores the maximum value that the counter can hold
+		int minV;		// The minimum value that the counter can hold
+		int incr;		// The step of the increment
+		int decr;		// The step of the decrement
+		int value;		// The value of the counter
+	public:
+		UpDownCounter();
+		UpDownCounter(int maxV, int minV, int incr, int decr);
+		int getValue();
+		void operator++();
+		void operator++(int dummy);
+		void operator--();
+		void operator--(int dummy);
+	};
+#endif
